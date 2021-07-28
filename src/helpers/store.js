15b12d03b1dd7e43f92725920af2d7ac2f70fa40
njm222 <nathan.john.menezes@gmail.com>
2021-07-28 13:38:19 +0530

    persist audioAnalyzerOptions

--- src/helpers/store.js
+++ src/helpers/store.js
@@ -2,6 +2,13 @@ import create from 'zustand'
 import { persist } from 'zustand/middleware'
 import SpotifyAnalyzer from '@/helpers/SpotifyAnalyzer'
 
+const defaultAnalyzerOptions = {
+  fftSize: 128,
+  smoothingTimeConstant: 0.8,
+  minDecibels: -90,
+  maxDecibels: -25,
+}
+
 const useStore = create(
   persist(
     (set) => ({
@@ -14,6 +21,7 @@ const useStore = create(
       isVisualizer: false,
       colourKey: 0,
       modeKey: 0,
+      audioAnalyzerOptions: defaultAnalyzerOptions,
       audioAnalyzer: null,
       spotifyAnalyzer: new SpotifyAnalyzer(),
       spotifyFeatures: null,
@@ -25,7 +33,7 @@ const useStore = create(
     {
       name: 'tessellator-zustand',
       version: 1,
-      whitelist: ['accessToken', 'refreshToken'],
+      whitelist: ['accessToken', 'refreshToken', 'audioAnalyzerOptions'],
       merge: (persistedState, currentState) => {
         if (currentState?.refreshToken) {
           return currentState
